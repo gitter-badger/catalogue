f29969faa0f10f63aadd3084ef596b8cb8be1746
Ricardo Ribeiro <ribeiro.r@ua.pt>
2014-12-12 16:30:34 +0000

    Fixed #754: - Made the data tabular, making it easier com compare databases.

--- emif/dashboard/static/js/vendor/widgets/widget.admin.general.stats.js
+++ emif/dashboard/static/js/vendor/widgets/widget.admin.general.stats.js
@@ -24,12 +24,41 @@ var FingerprintSchemasStatsWidget = function FingerprintSchemasStatsWidget(widge
     FingerprintSchemasStatsWidget._base.apply(this, [widgetname, "Database Types - Statistics", width, height, pos_x, pos_y]);
 
 }.inherit(DashboardWidget).addToPrototype({
+    __renderTable : function(table){
+        var tmp = '<table class="table table-bordered">';
+        var len = table.length;
+
+        for(var i=0;i<len;i++){
+            tmp += "<tr>";
+
+            var prop = table[i];
+            var proplen = prop.length;
+            for(var j=0;j<proplen;j++){
+                if(i==0){
+                    tmp+='<th><small>'+prop[j]+'</small></th>';
+                } else {
+                    if(j==0){
+                        tmp+='<td style="width: 40%;"><small>'+prop[j]+'</small></td>';
+                    } else {
+                        tmp+='<td>'+prop[j]+'</td>';
+                    }
+                }
+            }
+
+            tmp += "</tr>";
+        }
+        tmp += '</table>';
+
+        return tmp;
+    },
     __init : function(gridster, parent){
         var self = this;
 
         self.icon = '<i class="fa fa-line-chart"></i>';
 
         self.content = "<center><h3>Loading...</h3></center>";
+
+        self.header_style = "background-color: #d79494; border: 1px solid #b74c4c;";
 
         FingerprintSchemasStatsWidget._super.__init.apply(self, [gridster, parent]);
 
@@ -41,31 +70,53 @@ var FingerprintSchemasStatsWidget = function FingerprintSchemasStatsWidget(widge
                 self.db_types = [];
             }
             self.content = "";
+            var dbs = data['types'].length-1;
+
+            var table = [
+                ['Database'],
+                ['Total databases'],['Total Database owners'],['Total Shared Users'],
+                ['Max DB Shared'],['Avg DB Shared'], ['Total filled questions'], ['Max filled questions'],
+                ['Average filled questions'],
+                ['Total databases users (including shared and database owners)'],
+                ['Total interested users (all profiles)']
+            ];
+            var len = table.length;
             $.each(data['types'], function (db){
 
                 $.get("api/statistics/"+data['types'][db].id+"/all/all/all")
                 .done(function(dataJson) {
-                    self.content += "<strong>"+data['types'][db].name+"</strong> <br/>";
+
                     if(dataJson.stats){
 
-                        self.content += "Total databases: " + dataJson.stats.totalDatabases + "</br>";
-                        self.content += "Total Database owners: " + dataJson.stats.totalDatabaseOwners +"</br>";;
-                        self.content += "Total Shared Users: " + dataJson.stats.totalDatabaseShared + "</br>";
-                        self.content += "Max DB Shared: " + dataJson.stats.maxDatabaseShared + "</br>";
-                        self.content += "Avg DB Shared: " + dataJson.stats.avgDatabaseShared + "</br>";
-                        self.content += "Total filled questions: " + dataJson.stats.totalFilledQuestions+ "</br>";
-                        self.content += "Max filled questions: " + dataJson.stats.maxFilledFingerprints+ "</br>";
-                        self.content += "Average filled questions: " + dataJson.stats.avgFilledFingerprints+ "</br>";
-                        self.content += "Total databases users (including shared and database owners): " + dataJson.stats.totalDatabaseUsers+ "</br>";
-                        self.content += "Total interested users (all profiles): " + dataJson.stats.totalInterested+ "</br>";
+                        for(var i=0;i<len;i++){
+                            var tmp="";
 
+                            switch(i){
+                                case 0: tmp = data['types'][db].name; break;
+                                case 1: tmp = dataJson.stats.totalDatabases; break;
+                                case 2: tmp = dataJson.stats.totalDatabaseOwners; break;
+                                case 3: tmp = dataJson.stats.totalDatabaseShared; break;
+                                case 4: tmp = dataJson.stats.maxDatabaseShared; break;
+                                case 5: tmp = dataJson.stats.avgDatabaseShared; break;
+                                case 6: tmp = dataJson.stats.totalFilledQuestions; break;
+                                case 7: tmp = dataJson.stats.maxFilledFingerprints; break;
+                                case 8: tmp = dataJson.stats.avgFilledFingerprints; break;
+                                case 9: tmp = dataJson.stats.totalDatabaseUsers; break;
+                                case 10: tmp = dataJson.stats.totalInterested; break;
+                                default: tmp = "error"; break;
 
-
+                            }
+                            table[i].push(tmp);
+                        }
                     } else {
                         self.content = "Error Loading User Statistics Widget";
                     }
-                    FingerprintSchemasStatsWidget._super.__refresh.apply(self);
+                    if(dbs == db){
+                        self.content+= self.__renderTable(table);
+                    }
 
+                    FingerprintSchemasStatsWidget._super.__refresh.apply(self);
+                    $('.table', $('#'+self.widgetname)).parent().css('padding', '0px');
 
                   })
                 .fail(function() {
@@ -75,7 +126,6 @@ var FingerprintSchemasStatsWidget = function FingerprintSchemasStatsWidget(widge
                 });
 
             });
-
 
         })
         .fail(function() {
